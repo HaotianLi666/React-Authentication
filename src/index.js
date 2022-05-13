3721dd0b980e81c71a603c1a7a93c3bc74df33c1
Haotian Li <li000646@algonquinlive.com>
2022-05-13 15:10:47 -0400

    Create vars to store domain and clientId

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Now we can use our provider after we implement the env variable
+import { Auth0Provider } from '@auth0/auth0-react';
+
+//Create two variable to store our environmental variable
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //We will be able to access all Auth0Provider properties within our app
+  //Because Auth0Provider component uses React context
+  <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
