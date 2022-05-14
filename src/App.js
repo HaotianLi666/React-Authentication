da0c52666a4e88d0df3df4056ff38f76b274cef2
Haotian Li <li000646@algonquinlive.com>
2022-05-14 10:51:33 -0400

    determine if the webpage is loading

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
-  return (
+  const { isLoading } = useAuth0()
+
+  if(isLoading) return <div> Loading... </div>
+    return (
     <>
     <LoginButton />
     <LogoutButton/>
